014b7b6319c30f6ac54c0c76fd888413242dd080
Marcus Rands <mrands@hotmail.co.uk>
2017-10-31 15:54:54 +0000

    Written a series of tests to confirm that all aspects of the Update-quality method are working

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,56 +1,64 @@
 require_relative 'item.rb'
 
-class GildedRose
+class GildedRose #Keep track of inventry item quality
 
-  def initialize(items)
+  attr_reader :items
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
+  MIN_SELL_IN = 0
+
+  def initialize(items)#Class is presumably initialized with a list of items (inventry)
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+  def update_quality #Method to run at the end of each day to update quality
+    @items.each do |item| #Breaks item list in to individual components.
+
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" #For anything other than Brie or Backstage Pass, do the following.
+        if item.quality > 0 # If item quality is greater than 0, do the following.
+          if item.name != "Sulfuras, Hand of Ragnaros" #If the item is not Sulfuras, do the following.
+            item.quality = item.quality - 1 #Decrease item quality by 1.
           end
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+        if item.quality < 50 #This block relates purely to Brie and backstage passes.
+          item.quality = item.quality + 1 #quality is increased by 1 per day.
+          if item.name == "Backstage passes to a TAFKAL80ETC concert" #If item is a backstage pass.
+            if item.sell_in < 11 # and the sell_in is 10 or below...
+              if item.quality < 50 # And quality is below 50..
+                item.quality = item.quality + 1 #Increase quality by 1 when method is run.
               end
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
+            if item.sell_in < 6 #if backstage pass has less than 6 days to sell...
+              if item.quality < 50 # And is less than 50 quality.
+                item.quality = item.quality + 1 #Increase quality by 1.
               end
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+      if item.name != "Sulfuras, Hand of Ragnaros" #If item name is anything other than Sulfuras...
+        item.sell_in = item.sell_in - 1 #decrease the sell in date by 1.
       end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+
+      if item.sell_in < 0 #If the sell in date is lower than 0
+        if item.name != "Aged Brie" #If item is not Brie..
+          if item.name != "Backstage passes to a TAFKAL80ETC concert" #If item name is not backstage pass.
+            if item.quality > 0 #If quality is above 0
+              if item.name != "Sulfuras, Hand of Ragnaros" #If name is not Sulfuras.
+                item.quality = item.quality - 1 #Decrease the item quality by 1.
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = item.quality - item.quality #Sets item quality to 0 for backstage pass when sell_in date has passed.
           end
-        else
-          if item.quality < 50
+        else #This relates to aged brie, iem quality an increase as far as 50
+          if item.quality < 50 #
             item.quality = item.quality + 1
           end
         end
       end
+
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,78 @@
 require './lib/gilded_rose.rb'
 
 describe GildedRose do
+  subject(:rose) {described_class.new(items)}
+  let(:items) { [Item.new('golden turnip', 10, 5),
+                 Item.new('Aged Brie', 2, 0),
+                 Item.new('Sulfuras, Hand of Ragnaros', 0, 80),
+                 Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 40),
+                 Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 40),
+                 Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 40),
+                 Item.new('boring normal item', 0, 5)]}
+
+  it 'is instantiated with an items list' do
+    expect(rose.items).to eq items
+  end
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      rose.update_quality
+      expect(rose.items[0].name).to eq "golden turnip"
+    end
+
+    it 'reduces the sell_in value by 1 for all items' do
+      rose.update_quality
+      expect(rose.items[0].sell_in).to eq 9
+    end
+
+    it 'reduces the quality of normal items by 1 if there sell_in value is > 0' do
+      rose.update_quality
+      expect(rose.items[0].quality).to eq 4
+    end
+
+    it 'reduces the quality of normal items by 2 if sell_in value < 0' do
+      rose.update_quality
+      expect(rose.items[6].quality).to eq 3
+    end
+
+    it 'doesn\'t reduce the quality of an item below 0' do
+      6.times { rose.update_quality }
+      expect(rose.items[0].quality).to eq 0
+    end
+
+    it 'increase the quality of Aged Brie by 1 if its quality value is below 50' do
+      rose.update_quality
+      expect(rose.items[1].quality).to eq 1
+    end
+
+    it 'no longer increase the quality of brie when it has reached 50' do
+      51.times { rose.update_quality }
+      expect(rose.items[1].quality).to eq 50
+    end
+
+    it 'does not change the quality of Sulfuras' do
+      rose.update_quality
+      expect(rose.items[2].quality).to eq 80
+    end
+
+    it 'increase the quality of backstage passes by 1 when sell_in > 10 ' do
+      rose.update_quality
+      expect(rose.items[3].quality).to eq 41
+    end
+
+    it 'increase the quality of backstage passes by 2 when sell_in < 10 & > 5' do
+      rose.update_quality
+      expect(rose.items[4].quality).to eq 42
+    end
+
+    it 'increase the quality of backstage passes by 3 when sell_in < 5' do
+      rose.update_quality
+      expect(rose.items[5].quality).to eq 43
+    end
+
+    it 'reduces the quality of backstage passes to 0 when sell_in < 0' do
+      6.times { rose.update_quality }
+      expect(rose.items[5].quality).to eq 0
     end
   end
-
 end
